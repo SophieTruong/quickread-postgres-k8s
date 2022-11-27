801f7c62483cd2d9d1e1185528f6002b7e9919e5
SophieTruong <sophietruong1992@gmail.com>
2022-11-27 03:48:17 +0200

    Add tests (unit and integration), and add git workflow yaml file

--- services/flaskapp/src/config.py
+++ services/flaskapp/src/config.py
@@ -5,6 +5,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     """
     tbd
--- services/flaskapp/src/models.py
+++ services/flaskapp/src/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class UnlabeledData(db.Model):
     """
     unlabeled_data table:
@@ -10,5 +11,13 @@ class UnlabeledData(db.Model):
     """
     __tablename__ = "unlabeled_data"
     id = db.Column(db.Integer, primary_key=True)
-    raw_text_input = db.Column(db.String(10000000), unique=True, nullable=False)
+    raw_text_input = db.Column(db.String(10000000),
+                               unique=True, nullable=False)
     model_output = db.Column(db.String(10000000))
+
+    def __init__(self, raw_text_input: str, model_output: str):
+        """Create a new User object using the email address and hashing the
+        plaintext password using Werkzeug.Security.
+        """
+        self.raw_text_input = raw_text_input
+        self.model_output = model_output
